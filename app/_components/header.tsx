d1abec135eb90268099f4af3d89cec9842a0af88
Clepson <clepsonsantos9@gmail.com>
2024-02-14 22:48:26 -0300

    fix: adjustments.

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 
 const Header = () => {
     return (
-        <Card >
+        <Card className="rounded-none" >
             <CardContent className="p-5 justify-between items-center flex flex-row" >
                 <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
                 <Button variant="outline" size="icon" className="h-8 w-8">

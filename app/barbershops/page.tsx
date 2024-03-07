eddee71e9c3f660fd560eded59629fd3d9c63c5b
Clepson <clepsonsantos9@gmail.com>
2024-03-06 21:13:05 -0300

    fix: adjustment in the search for barbershops

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
 import BarbershopItem from "../(home)/_components/barbershop-item";
 import Header from "../_components/header";
 import { db } from "../_lib/prisma";
+import Search from "../(home)/_components/search";
 
 interface BarbershopsPageProps {
   searchParams: {
@@ -9,6 +11,10 @@ interface BarbershopsPageProps {
 }
 
 const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+
+  if (!searchParams.search) {
+    return redirect("/")
+  }
 
   const barbershops = await db.barbershop.findMany({
     where: {
@@ -23,15 +29,20 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
     <>
       <Header />
 
-      <div className="px-5 py-6">
+
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search defaultValues={{
+          search: searchParams.search
+        }} />
+
         <h1 className="text-gray-400 font-bold text-xs uppercase">
           Resultados para &quot;{searchParams.search}&quot;
         </h1>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mt-3 gap-4">
           {barbershops.map((barbershop) => (
-            <div className="w-full">
-              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            <div key={barbershop.id} className="w-full">
+              <BarbershopItem barbershop={barbershop} />
             </div>
           ))}
         </div>
